b4211376cec30f78a6029f1e08130f44fda8c755
Malchazeen <malchazeen.edge@gmail.com>
2020-04-14 18:26:11 +0700

    6

--- wwwroot/js/creature.js
+++ wwwroot/js/creature.js
@@ -1,5 +1,6 @@
 ﻿import Mustache from './mustache.js';
-import tippy from './tippy.esm.js';
+import { delegate } from './tippy.esm.js';
+import './event-delegation.js';
 
 (async function () {
     const queryString = new URLSearchParams(window.location.search);
@@ -22,49 +23,165 @@ import tippy from './tippy.esm.js';
         const creatureData = data[4];
 
         document.title = creatureData.Outline.NickName;
-        const creature = new Creature(map, creatureData);
-        document.body.appendChild(creature.container);
-        creature.activeTab(fragment);
+        const container = (function (map, creatureData, fragment) {
+            const container = document.createElement('div');
+            container.id = 'container';
+            container.innerHTML = document.querySelector('template[name=Container]').innerHTML;
+
+            container.querySelector('.outline').innerHTML = Mustache.render(document.querySelector('template[name=Outline]').innerHTML, creatureData.Outline);
+            const content = container.querySelector('.content');
+            
+            delegate(content, {
+                target: '.tooltip',
+                content: 'Loading...',
+                arrow: false,
+                allowHTML: true,
+                theme: 'custom',
+                onShow: instance => {
+                    const element = instance.reference;
+                    const html = Mustache.render(document.querySelector(`template[name=${element.dataset.template}]`).innerHTML, map.get(activeTab.getAttribute('name'))[element.getAttribute('name')]);
+                    instance.setContent(html);
+                },
+                onHidden: instance => {
+                    instance.setContent('Loading...');
+                }
+            });
+
+            const tab = container.querySelector('.tab');
+            tab.addEventListener('click', e => {
+                const button = e.target.closest('.tab-button');
+                if (!button)
+                    return;
+
+                activeTab.classList.remove('active-tab');
+                activateTab(button);
+            });
+
+            const tabs = new Map();
+            tab.querySelectorAll('.tab-button')
+                .forEach(v => tabs.set(v.getAttribute('name'), v));
+            let activeTab = undefined;
+            activateTab(tabs.get(fragment));
+
+            return container;
+
+            function activateTab(button) {
+                const name = button.getAttribute('name');
+                button.classList.add('active-tab');
+                activeTab = button;
+                const tabContent = GetContent();
+                tabContent.renderContent();
+                tabContent.setupEventListeners();
+
+                function GetContent() {
+                    switch (name) {
+                        case 'Skills':
+                            return new Skills(content, creatureData.Detail, map.get(name));
+                        default:
+                            return new TabContent(content, name, creatureData.Detail);
+                    }
+                }
+            }
+        })(map, creatureData, fragment);
+        document.body.appendChild(container);
     }
 })();
 
-class Creature {
-    constructor(map, creatureData) {
-        this.map = map;
-        this.creatureData = creatureData;
+class TabContent {
+    constructor(content, name, data) {
+        this.content = content;
+        this.name = name;
+        this.data = data;
+    }
 
-        this.container = document.createElement('div');
-        this.container.id = 'container';
-        this.container.innerHTML = document.querySelector('template[name=Container]').innerHTML;
+    get template() {
+        return document.querySelector(`template[name=${this.name}]`).innerHTML;
+    }
 
-        this.container.querySelector('.outline').innerHTML = Mustache.render(document.querySelector('template[name=Outline]').innerHTML, creatureData.Outline);
+    renderContent() {
+        this.content.innerHTML = Mustache.render(this.template, this.data);
+    }
 
-        this.tab = this.container.querySelector('.tab');
-        this.content = this.container.querySelector('.content');
+    setupEventListeners() {
+    }
+}
 
-        this.tab.querySelectorAll('.tab-button').forEach(x => {
-            x.addEventListener('click', e => this.activeTab(e.target.getAttribute('name')));
+class TabContentWithFilter extends TabContent {
+    constructor(content, name, data) {
+        super(content, name, data);
+    }
+
+    setupEventListeners() {
+        this.content.on('change', 'div.filter > .all label.checkbox > input[type=checkbox]', e => {
+            if (this.content.querySelectorAll('div.filter > .specific label.checkbox > input[type=checkbox]:checked').length === 0)
+                e.target.checked = true;
+            else
+                e.target.checked = false;
+            e.target.indeterminate = false;
+
+            this.content.querySelectorAll('div.filter > .specific label.checkbox > input[type=checkbox]').forEach(v => v.checked = e.target.checked);
+
+            this.filterChanged();
+        });
+
+        this.content.on('change', 'div.filter > .specific label.checkbox', e => {
+            const checkedCount = this.content.querySelectorAll('div.filter > .specific label.checkbox > input[type=checkbox]:checked').length;
+            const checkboxAll = this.content.querySelector('div.filter > .all label.checkbox > input[type=checkbox]');
+            if (checkedCount === 0)
+                checkboxAll.indeterminate = checkboxAll.checked = false;
+            else if (checkedCount < this.content.querySelectorAll('div.filter > .specific label.checkbox > input[type=checkbox]').length)
+                checkboxAll.indeterminate = true;
+            else {
+                checkboxAll.checked = true;
+                checkboxAll.indeterminate = false;
+            }
+
+            this.filterChanged();
         });
     }
 
-    activeTab(name) {
-        this.tab.querySelectorAll('.tab-button').forEach(x => x.classList.remove('active-tab'));
-        this.tab.querySelector(`.tab-button[name=${name}]`).classList.add('active-tab');
-        this.content.innerHTML = Mustache.render(document.querySelector(`template[name=${name}]`).innerHTML, this.creatureData.Detail);
+    renderContent() {
+        super.renderContent();
+        this.renderFilter();
+        this.renderBody();
+    }
 
-        tippy(document.querySelectorAll('.tooltip'), {
-            content: 'Loading...',
-            arrow: false,
-            allowHTML: true,
-            theme: 'custom',
-            onShow: instance => {
-                const element = instance.reference;
-                const html = Mustache.render(document.querySelector(`template[name=${element.dataset.template}]`).innerHTML, this.map.get(name)[element.getAttribute('name')]);
-                instance.setContent(html);
-            },
-            onHidden: instance => {
-                instance.setContent('Loading...');
-            }
+    renderFilter() {
+        this.content.querySelector('div.tabular-section.filter').innerHTML = Mustache.render(document.querySelector(`template[name=ContentFilter]`).innerHTML, this.data);
+        this.content.querySelectorAll('div.tabular-section.filter label.checkbox > input[type=checkbox]').forEach(v => v.checked = true);
+    }
+
+    renderBody() {
+        this.content.querySelector('div.tabular-section.associative-array').innerHTML = Mustache.render(document.querySelector(`template[name=SkillsBody]`).innerHTML, this.data);
+    }
+
+    filterChanged() {
+    }
+}
+
+class Skills extends TabContentWithFilter {
+    constructor(content, creatureDetail, skills) {
+        const tags = new Set();
+        creatureDetail.Skills.forEach(x => {
+            skills[x.ID].Tags.forEach(y => {
+                if (!tags.has(y))
+                    tags.add(y);
+            });
         });
+        super(content, 'Skills', { Filter: [...tags], Skills: creatureDetail.Skills.slice() });
+        this.skills = skills;
+        this.tags = tags;
+        this.creatureSkills = creatureDetail.Skills;
+    }
+
+    filterChanged() {
+        const tags = new Set();
+        this.content.querySelectorAll('div.filter > .specific label.checkbox > input[type=checkbox]:checked').forEach(v => {
+            const id = v.closest('label.checkbox').getAttribute('name');
+            if (!tags.has(id))
+                tags.add(id);
+        });
+        this.data.Skills = this.creatureSkills.filter(x => this.skills[x.ID].Tags.some(y => tags.has(y)));
+        this.renderBody();
     }
 }
---
+++ wwwroot/js/event-delegation.js
@@ -0,0 +1,10 @@
+﻿Element.prototype.on = function (event, selector, handler) {
+    this.addEventListener(event, function (e) {
+        for (var target = e.target; target && target !== this; target = target.parentNode) {
+            if (target.matches(selector)) {
+                handler.call(target, e);
+                break;
+            }
+        }
+    });
+};
